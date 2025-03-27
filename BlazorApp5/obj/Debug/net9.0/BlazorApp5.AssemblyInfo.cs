124ce17bcc87031fc972568d03ce523e086b47cc
Санечик <idskisatana666@podushkaperdushka.pahan>
2025-03-27 19:24:54 +0400

    переделал дизайн calc

--- BlazorApp5/obj/Debug/net9.0/BlazorApp5.AssemblyInfo.cs
+++ BlazorApp5/obj/Debug/net9.0/BlazorApp5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04a9c370b325da38edd79ece8756bb499fb0203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5693562a2c771d07f4869efc4e47bc249601161a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
